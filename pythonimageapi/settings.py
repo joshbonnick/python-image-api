1e9c1537e3bae7f5467805fdffd860c603cd343e
Josh Bonnick <jbonnick@acquiredigital.com>
2026-05-02 20:49:20 +0100

    Use db in database dir

--- pythonimageapi/settings.py
+++ pythonimageapi/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'pythonimageapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database/data.sqlite3',
     }
 }
 
