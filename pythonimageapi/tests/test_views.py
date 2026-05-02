f65c7929242d059b3c99a94a8dd398e4768de825
Josh Bonnick <jbonnick@acquiredigital.com>
2026-05-02 22:54:14 +0100

    Test passing image width/height

--- pythonimageapi/tests/test_views.py
+++ pythonimageapi/tests/test_views.py
@@ -12,7 +12,7 @@ class ViewsTestCase(TestCase):
 
     def test_svg_returns_svg_response(self):
         request = self.factory.get("/svg/200/ff0000", data={"text": "::foo::"})
-        response = images.svg(request, 200, "ff0000")
+        response = images.svg(request, "200", "ff0000")
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('content-type'), 'image/svg+xml')
@@ -24,6 +24,20 @@ class ViewsTestCase(TestCase):
         self.assertIn('height="200"', svg)
         self.assertIn('fill="#ff0000"', svg)
         self.assertIn('::foo::', svg)
+
+        request = self.factory.get("/svg/300x400/00ff00")
+        response = images.svg(request, "300x400", "00ff00")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get('content-type'), 'image/svg+xml')
+
+        svg = response.content.decode('utf-8')
+
+        self.assertIn('<svg', svg)
+        self.assertIn('width="300"', svg)
+        self.assertIn('height="400"', svg)
+        self.assertIn('fill="#00ff00"', svg)
+        self.assertNotIn('<text', svg)
 
 if __name__ == '__main__':
     unittest.main()
--- pythonimageapi/views/images.py
+++ pythonimageapi/views/images.py
@@ -3,8 +3,20 @@ from django.http import HttpResponse
 from pythonimageapi.images.svg_generator import SvgGenerator
 
 
-def svg(request, size: int, color: str):
+def svg(request, size: str, color: str):
     generator = SvgGenerator()
 
-    return HttpResponse(generator.generate_svg(int(size), int(size), color, request.GET['text']),
-                        content_type='image/svg+xml')
+    if "x" in size:
+        parts = size.split('x')
+
+        width = int(parts[0])
+        height = int(parts[1])
+    else:
+        width = int(size)
+        height = int(size)
+
+    text = None
+    if 'text' in request.GET:
+        text = request.GET['text']
+
+    return HttpResponse(generator.generate_svg(width, height, color, text), content_type='image/svg+xml')
