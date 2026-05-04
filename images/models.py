91ca41cf30faceedca81ce634b86d8794a0b5433
Josh Bonnick <jbonnick@acquiredigital.com>
2026-05-04 18:00:42 +0100

    create migrations for image generation model

--- images/models.py
+++ images/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ImageGeneration(models.Model):
     class ImageType(models.TextChoices):
-        SVG = "SVG", "Scalable Vector Graphic"
+        SVG = "SVG"
 
     type = models.CharField(max_length=4, choices=ImageType, db_index=True)
     size = models.CharField(max_length=255)
