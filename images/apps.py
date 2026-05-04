53aeebc23cb844c688c42b5f0ade9ba343178e53
Josh Bonnick <jbonnick@acquiredigital.com>
2026-05-04 18:24:07 +0100

    register `ImageGeneration` for admin panel

--- images/apps.py
+++ images/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from django.contrib import admin
 
 class ImagesConfig(AppConfig):
     name = 'images'
+
+    def ready(self):
+        from .models import ImageGeneration
+        admin.site.register(ImageGeneration)
