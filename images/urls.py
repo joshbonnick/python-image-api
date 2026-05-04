ce607dbc411babab4bfe6cc3d55a7b7235afbec8
Josh Bonnick <jbonnick@acquiredigital.com>
2026-05-04 18:30:12 +0100

    update URL patterns in `images/urls.py` to use `<str>` converters

--- images/urls.py
+++ images/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import SvgImages
 
 urlpatterns = [
-    path('svg/<path:size>/<path:color>', SvgImages.as_view()),
-    path('svg/<path:size>', SvgImages.as_view()),
+    path('svg/<str:size>/<str:color>', SvgImages.as_view()),
+    path('svg/<str:size>', SvgImages.as_view()),
     path('svg/', SvgImages.as_view()),
 ]
